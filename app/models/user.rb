ec5d3624f283bb8623f145940d35dc7162c09209
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-05-02 09:04:34 +0000

    [Update]いいね機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image, destroy: false
 
+  has_many :favorites, dependent: :destroy
+
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
 
