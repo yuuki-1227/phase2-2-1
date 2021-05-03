dccb1d3afe64fef23d46dc607ef556401d91112f
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-05-03 02:16:10 +0000

    [Update]

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users,only: [:show,:index,:edit,:update] do
-    menber do
+    member do
       get :follower, :followed
     end
   end
