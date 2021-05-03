27dcd47a7bb38fe5739180e9f8e91933ab4fd6ec
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-05-03 00:52:03 +0000

    [Update]user index

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,9 +1,17 @@
 class RelationshipsController < ApplicationController
 
   def create
+    user = User.find(params[:user_id])
+    relationship = current_user.relationships.new(user_id: user.id)
+    relationship.save
+    redirect_to users_path
   end
 
   def destroy
+    user = User.find(params[:user_id])
+    relationship = current_user.relationships.find_by(user_id: user.id)
+    relationship.destroy
+    redirect_to users_path
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users,only: [:show,:index,:edit,:update] do
-    resources :relationships, only: [:create, :destroy]
+    resource :relationships, only: [:create, :destroy]
   end
 
   resources :books do
