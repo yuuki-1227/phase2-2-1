b735b769718914ba89353ec10a73ce501cebdc64
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-05-05 07:19:30 +0000

    [Update]phase2-6

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,19 +1,26 @@
 class FavoritesController < ApplicationController
 
-
+  before_action :set_variables
 
   def create
     @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: @book.id)
-    favorite.save
+    @favorite = current_user.favorites.new(book_id: @book.id)
+    @favorite.save
     redirect_back fallback_location: root_path
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: @book.id)
-    favorite.destroy
+    @favorite = current_user.favorites.find_by(book_id: @book.id)
+    @favorite.destroy
     redirect_back fallback_location: root_path
+  end
+
+  private
+
+  def set_variables
+    @book = Book.find(params[:book_id])
+    @id_name = "#favorite-link-#{@book.id}"
   end
 
 end
