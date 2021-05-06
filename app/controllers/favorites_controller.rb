1e139399c912e4adf05b31abef38c0e67408496a
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-05-06 02:33:16 +0000

    [Update]

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,14 @@ class FavoritesController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    @favorite = current_user.favorites.build(book_id: params[:book_id])
-    @favorite.save
+    favorite = current_user.favorites.build(book_id: params[:book_id])
+    favorite.save
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-    @favorite = Favorite.find_by(book_id: params[:book_id], user_id: current_user.id)
-    @favorite.destroy
+    favorite = Favorite.find_by(book_id: params[:book_id], user_id: current_user.id)
+    favorite.destroy
   end
 
   private
