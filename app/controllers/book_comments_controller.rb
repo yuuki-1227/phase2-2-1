0c5c5683f76c1fb8d5d84ad11d954416e4583459
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-05-06 08:20:14 +0000

    [Update]コメント機能

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,17 +2,20 @@ class BookCommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = @book.id
-    comment.save
-    redirect_to book_path(@book)
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    if @book_comment.save
 
+    else
+      @user = @book.user
+      @book_new = Book.new
+      render template "books/show"
+    end
   end
 
 
   def destroy
     BookComment.find_by(id: params[:id],book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
   end
 
   private
